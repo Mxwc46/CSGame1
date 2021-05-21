72f4679d5d03b3a1c76ea2b1736c8fef5da6aa41
Max Callon <T0070221@cardinalnewman.ac.uk>
2021-05-21 14:00:23 +0100

    particles

--- CSGame1/Content/Load/GameplayScreen.cs
+++ CSGame1/Content/Load/GameplayScreen.cs
@@ -6,11 +6,23 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
+using MonoGame.Extended;
+using MonoGame.Extended.Particles;
+using MonoGame.Extended.Particles.Modifiers;
+using MonoGame.Extended.Particles.Modifiers.Containers;
+using MonoGame.Extended.Particles.Modifiers.Interpolators;
+using MonoGame.Extended.Particles.Profiles;
+using MonoGame.Extended.TextureAtlases;
+
 namespace CSGame1
 
 {
+
+
     public class GameplayScreen : GameScreen
     {
+        private ParticleEffect _particleEffect;
+        private Texture2D _particleTexture;
         //cringe
         Player player;
         public override void LoadContent()
@@ -20,6 +32,46 @@ namespace CSGame1
             XmlManager<Player> playerLoader = new XmlManager<Player>();
             player = playerLoader.Load("Content/Load/Player.xml");
             player.LoadContent();
+
+            _particleTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _particleTexture.SetData(new[] { Color.White });
+
+            TextureRegion2D textureRegion = new TextureRegion2D(_particleTexture);
+            _particleEffect = new ParticleEffect(autoTrigger: false)
+            {
+                Position = new Vector2(400, 240),
+                Emitters = new List<ParticleEmitter>
+    {
+        new ParticleEmitter(textureRegion, 500, TimeSpan.FromSeconds(2.5),
+            Profile.BoxUniform(100,250))
+        {
+            Parameters = new ParticleReleaseParameters
+            {
+                Speed = new Range<float>(0f, 50f),
+                Quantity = 3,
+                Rotation = new Range<float>(-1f, 1f),
+                Scale = new Range<float>(3.0f, 4.0f)
+            },
+            Modifiers =
+            {
+                new AgeModifier
+                {
+                    Interpolators =
+                    {
+                        new ColorInterpolator
+                        {
+                            StartValue = new HslColor(0.33f, 0.5f, 0.5f),
+                            EndValue = new HslColor(0.5f, 0.9f, 1.0f)
+                        }
+                    }
+                },
+                new RotationModifier {RotationRate = -2.1f},
+                new RectangleContainerModifier {Width = 800, Height = 480},
+                new LinearGravityModifier {Direction = -Vector2.UnitY, Strength = 30f},
+            }
+        }
+    }
+            };
         }
 
         public override void UnloadContent()
---
+++ CSGame1/Content/Load/Particles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MonoGame.Extended;
+using MonoGame.Extended.Particles;
+using MonoGame.Extended.Particles.Modifiers;
+using MonoGame.Extended.Particles.Modifiers.Containers;
+using MonoGame.Extended.Particles.Modifiers.Interpolators;
+using MonoGame.Extended.Particles.Profiles;
+using MonoGame.Extended.TextureAtlases;
+
+namespace CSGame1.Content.Load
+{
+    class Particles
+    {
+    }
+}
