1f7f2fe3766ca8d949dd88af3728832f1c4b2601
Max Callon <T0070221@cardinalnewman.ac.uk>
2021-06-24 13:18:12 +0100

    menu

--- CSGame1/ScreenManager.cs
+++ CSGame1/ScreenManager.cs
@@ -77,10 +77,10 @@ namespace CSGame1
         public ScreenManager()
         {
             Dimensions = new Vector2(640, 480);
-            currentScreen = new GameplayScreen();
+            currentScreen = new SplashScreen();
             xmlGameScreenManager = new XmlManager<GameScreen>();
             xmlGameScreenManager.Type = currentScreen.Type;
-            //currentScreen = xmlGameScreenManager.Load("Content/Load/SplashScreen.xml");
+            currentScreen = xmlGameScreenManager.Load("Content/Load/SplashScreen.xml");
         }
         public void LoadContent(ContentManager Content)
         {
